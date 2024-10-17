ae06e9f346c0e514c7fec051fabd4a1970f0cd60
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2024-10-18 00:09:46 +0200

    feat(BoxData): Added the list with the info of all boxes.

---
+++ data/BoxData.tsx
@@ -0,0 +1,55 @@
+import { BoxProps } from "../components/BoxData";
+
+export const boxes: BoxProps[] = [
+  {
+    description: "Jugar",
+    image: require("../assets/SofyanAmrabat.jpg"),
+  },
+  {
+    description: "A",
+    image: require("../assets/SofyanAmrabat.jpg"),
+  },
+  {
+    description: "A",
+    image: require("../assets/SofyanAmrabat.jpg"),
+  },
+  {
+    description: "A",
+    image: require("../assets/SofyanAmrabat.jpg"),
+  },
+  {
+    description: "A",
+    image: require("../assets/SofyanAmrabat.jpg"),
+  },
+  {
+    description: "A",
+    image: require("../assets/SofyanAmrabat.jpg"),
+  },
+  {
+    description: "A",
+    image: require("../assets/SofyanAmrabat.jpg"),
+  },
+  {
+    description: "A",
+    image: require("../assets/SofyanAmrabat.jpg"),
+  },
+  {
+    description: "A",
+    image: require("../assets/SofyanAmrabat.jpg"),
+  },
+];
+
+{
+  /* <Text style={styles.cosasQmeGustanMuxoEstails}>Salir a pasear</Text>
+      <Text style={styles.cosasQmeGustanMuxoEstails}>Senderismo</Text>
+      <Text style={styles.cosasQmeGustanMuxoEstails}>Ir a la playita</Text>
+      <Text style={styles.cosasQmeGustanMuxoEstails}>Domingos de misa</Text>
+      <Text style={styles.cosasQmeGustanMuxoEstails}>La guitarrita</Text>
+      <Text style={styles.cosasQmeGustanMuxoEstails}>El monte con lluvia</Text>
+      <Text style={styles.cosasQmeGustanMuxoEstails}>Viajar</Text>
+      <Text style={styles.cosasQmeGustanMuxoEstails}>Música variadita</Text>
+      <Text style={styles.cosasQmeGustanMuxoEstails}>Anime</Text>
+      <Text style={styles.cosasQmeGustanMuxoEstails}>Ducharme</Text>
+      <Text style={styles.cosasQmeGustanMuxoEstails}>Videojuegos</Text>
+       */
+}
