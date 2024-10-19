352e6165a9e1eaa0d0d2ad91aa1e72e6bb9c5323
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2024-10-19 14:35:01 +0200

    feat(Card): Changed styles of the card with own styles.

--- components/Card.tsx
+++ components/Card.tsx
@@ -15,36 +15,53 @@ export type CardProps = {
 
 export const Card = ({ avatar, title, description }: CardProps) => {
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        alignItems: "center",
-        backgroundColor: "red",
-      }}
-    >
-      <Image style={styles.avatar} source={avatar}></Image>
-      <View
-        style={{
-          margin: 10,
-          backgroundColor: "lightgray",
-          padding: 10,
-          borderRadius: 10,
-          width: "70%",
-        }}
-      >
-        <Text style={{ textAlign: "center", fontWeight: "700", fontSize: 20 }}>
-          {title}
-        </Text>
-        <Text>{description}</Text>
+    <View style={styles.cardContainer}>
+      <View style={styles.cardAvatar}>
+        <Image style={styles.avatar} source={avatar} />
+      </View>
+      <View style={styles.cardInfo}>
+        <Text style={styles.cardTitle}>{title}</Text>
+        <Text style={styles.cardDescription}>{description}</Text>
       </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  cardContainer: {
+    display: "flex",
+    flexDirection: "column",
+    width: 380,
+    height: 230,
+    maxWidth: 380,
+  },
+  cardInfo: {
+    display: "flex",
+    flexDirection: "column",
+    backgroundColor: "lightgray",
+    margin: 10,
+    padding: 5,
+    borderRadius: 10,
+    maxWidth: 390,
+    height: 100,
+    maxHeight: 100,
+  },
+  cardAvatar: {
+    marginHorizontal: "auto",
+    paddingTop: 5,
+  },
+  cardTitle: {
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 22,
+  },
+  cardDescription: {
+    fontSize: 16,
+    textAlign: "justify",
+  },
   avatar: {
-    height: 90,
-    width: 90,
-    borderRadius: 100,
+    height: 100,
+    width: 100,
+    borderRadius: 30,
   },
 });
