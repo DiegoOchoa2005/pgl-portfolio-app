cc926c1be53fbb412aabb7a02af74c04a046f640
DiegoOchoa2005 <usr3858@salesianos-lacuesta.net>
2024-10-20 14:09:10 +0200

    feat(Color): Added light and dark theme colors

--- styles/Color.ts
+++ styles/Color.ts
@@ -4,3 +4,30 @@ export const COLORS = {
   borderPrimary: "#fff",
   backgroundColor: "red",
 };
+export const COLORS_DARK = {
+  backgroundPrimary: "#3E2723",
+  backgroundSecondary: "#5D4037",
+  backgroundTertiary: "#4E342E",
+  textPrimary: "#FFEBEE",
+  textSecondary: "#FFCDD2",
+  textDisabled: "#BCAAA4",
+  primaryColor: "#8D6E63",
+  secondaryColor: "#7E57C2",
+  tertiaryColor: "#4DB6AC",
+  borderColor: "#4E342E",
+  errorColor: "#C62828",
+};
+
+export const COLORS_LIGHT = {
+  backgroundPrimary: "#FFF7E6",
+  backgroundSecondary: "#FFE0B2",
+  backgroundTertiary: "#FFF0DB",
+  textPrimary: "#4E342E",
+  textSecondary: "#6D4C41",
+  textDisabled: "#A1887F",
+  primaryColor: "#FFCC80",
+  secondaryColor: "#B39DDB",
+  tertiaryColor: "#80CBC4",
+  borderColor: "#D7CCC8",
+  errorColor: "#E57373",
+};
